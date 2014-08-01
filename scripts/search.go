6eee0be57e783cd6b87f6c7c02c4e6b8a4d0a9af
keimoon <http.sslverify>
2014-08-02 02:50:15 +0700

    Add search script

---
+++ scripts/search.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"fmt"
+	"github.com/dancannon/gorethink"
+	"math"
+	"regexp"
+	"strings"
+	"sort"
+)
+
+var replaceRe = regexp.MustCompile("\\(|\\)|\"|'|_|\\-")
+var splitRe = regexp.MustCompile("\\s+")
+
+func tokenizer(title string) []string {
+	title = replaceRe.ReplaceAllString(strings.ToLower(title), "")
+	return splitRe.Split(title, -1)
+}
+
+func ngram(tokens []string, n int) []string {
+	i := 0
+	grams := []string{}
+	limit := len(tokens) - (n - 1)
+	for i < limit {
+		grams = append(grams, strings.Join(tokens[i:i+n], " "))
+		i++
+	}
+	return grams
+}
+
+type Document struct {
+	Title string
+	Score float64
+}
+
+type DocumentList []*Document
+
+func (a DocumentList) Len() int {
+	return len(a)
+}
+
+func (a DocumentList) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+func (a DocumentList) Less(i, j int) bool {
+	return a[i].Score > a[j].Score
+}
+
+type Title struct {
+	Id   int64  `gorethink:"id"`
+	Slug string `gorethink:"slug"`
+}
+
+type Index struct {
+	Id   string  `gorethink:"id"`
+	Docs []int64 `gorethink:"docs"`
+}
+
+var numDoc = 3500
+
+func search(session *gorethink.Session, query string) DocumentList {
+	tokens := tokenizer(query)
+	docs := make(map[int64]*Document)
+	for n := 1; n <= 3; n++ {
+		grams := ngram(tokens, n)
+		for _, gram := range grams {
+			rows, err := gorethink.Table("indexes").Get(gram).Run(session)
+			if err != nil {
+				continue
+			}
+			if rows.IsNil() {
+				rows.Close()
+				continue
+			}
+			idx := &Index{}
+			rows.One(idx)
+			for _, docId := range idx.Docs {
+				doc := docs[docId]
+				if doc == nil {
+					rows, err = gorethink.Table("titles").Get(docId).Run(session)
+					if err != nil {
+						continue
+					}
+					if rows.IsNil() {
+						rows.Close()
+						continue
+					}
+					title := &Title{}
+					rows.One(title)
+					doc = &Document{Score: 0, Title: title.Slug}
+				}
+				tf := float64(n) / float64(len(doc.Title))
+				idf := math.Log(float64(numDoc) / float64(len(idx.Docs)))
+				doc.Score += tf * idf * float64(n)
+				docs[docId] = doc
+			}
+		}
+	}
+	docList := DocumentList{}
+	for _, doc := range docs {
+		docList = append(docList, doc)
+	}
+	sort.Sort(docList)
+	return docList
+}
+
+func main() {
+	session, _ := gorethink.Connect(gorethink.ConnectOpts{
+		Address:  "127.1:28015",
+		Database: "wikivoice",
+	})
+	query := "a fire upon the deep"
+	documents := search(session, query)
+	for _, doc := range documents {
+		fmt.Println(doc)
+	}
+}
